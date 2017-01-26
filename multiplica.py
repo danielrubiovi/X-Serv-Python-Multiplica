a191fb3bf152e736a874a39aa2789720c0a550c1
Daniel Rubio Villegas <drubio@gamma37.aulas.gsyc.urjc.es>
2017-01-26 11:53:10 +0100

    Ejercicio Multiplicamultiplica

---
+++ multiplica.py
@@ -0,0 +1,7 @@
+'''Daniel Rubio Villegas'''
+
+for num1 in range(1,11):
+    print ("\nTabla del",num1,"\n------------")
+    for num2 in range(1,11):
+        resultado = num1 * num2
+        print(num1, "por", num2, "es", resultado)
